0955a2bf3a750cc6c2ceae9ca4f089e5d283ba24
Dawid Janiga <dawid.janiga@netguru.com>
2021-12-21 01:41:22 +0100

    refactor: remove empty client

--- libs/client/src/lib/client-npm.spec.ts
+++
@@ -1,7 +0,0 @@
-import { clientNpm } from './client-npm';
-
-describe('clientNpm', () => {
-  it('should work', () => {
-    expect(clientNpm()).toEqual('client-npm');
-  });
-});
--- libs/client/src/lib/client-npm.ts
+++
@@ -1,3 +0,0 @@
-export function clientNpm(): string {
-  return 'client-npm';
-}
