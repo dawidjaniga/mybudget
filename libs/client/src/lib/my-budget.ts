ce9fe3f17ae5d22772365edf2dc3cd6a787cd0db
Dawid Janiga <dawid.janiga@netguru.com>
2021-12-23 12:44:13 +0100

    feat(client): set fixed api url

--- libs/client/src/lib/my-budget.ts
+++ libs/client/src/lib/my-budget.ts
@@ -3,7 +3,9 @@ import wallet from './wallet'
 
 export function MyBudget () {
   const apiClient = axios.create({
-    baseURL: process.env.API_URL,
+    // baseURL: process.env.API_URL,
+    // @TODO: replace with inline process env replacer
+    baseURL: 'https://api-mybudget.herokuapp.com/api',
     headers: {
       'content-type': 'application/json'
     }
